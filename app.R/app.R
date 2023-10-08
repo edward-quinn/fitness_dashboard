6cb61ad4915da5bd3bf019a3cdd6dec638a5ae7b
edward-quinn <edward.bruce.quinn@gmail.com>
2023-10-08 11:09:36 -0400

    prototype is working

--- app.R/app.R
+++ app.R/app.R
@@ -3,6 +3,7 @@
 library(shiny)
 library(tidyverse)
 library(googlesheets4)
+library(bslib)
 
 df <- read_sheet("https://docs.google.com/spreadsheets/d/1bV5oIjOFwZYxEFU9NvOksjaCMsO1-4OXFUBMSmKrah4/edit#gid=0",
                  col_types = "icDiii") |> 
@@ -11,48 +12,42 @@ df <- read_sheet("https://docs.google.com/spreadsheets/d/1bV5oIjOFwZYxEFU9NvOksj
   
 
 
-
-
-
-
 # Define UI for application
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+ui <- page_sidebar(
+  title = "Fitness Dashboard",
+  sidebar = sidebar(
+    title = "Filters",
+    selectInput(
+      "technique", "Which lifting technique do you want to visualize?",
+      sort(unique(df$technique)),
+      multiple = TRUE),
+    dateRangeInput("dates", "Which dates do you want to visualize?",
+                   start = "2022-03-07",
+                   end = "2023-10-06")),
+  card(
+    card_header("Are there changes in weight lifted over time?"),
+    plotOutput("plot")
+  )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic 
 server <- function(input, output) {
 
+  selected <- reactive(
+    df |> 
+      filter(date >= input$dates[1] & date <= input$dates[2]) |> 
+      filter(technique %in% input$technique)
+  )
   
-  # ggplot(df |> 
-  #            drop_na() |>  
-  #            mutate(volume = repetitions*weight) |>  
-  #            ggplot(aes(x = date, y = volume, color = technique)) +
-  #            geom_point() +
-  #            geom_line() +
-  #            theme_bw() +
-  #            facet_wrap(~ set))
-  
+  output$plot <- renderPlot(
     
+    ggplot(selected(),
+                        aes(x = date, y = volume, color = technique)) +
+    geom_point() +
+    geom_line() +
+    facet_wrap(~ set)
+  )
 }
 
 # Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
